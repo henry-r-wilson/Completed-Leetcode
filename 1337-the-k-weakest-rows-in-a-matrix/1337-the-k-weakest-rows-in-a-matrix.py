a2490d9b100763cda969e6cb8e81283523aa7919
Henry Wilson <113286807+henry-r-wilson@users.noreply.github.com>
2022-09-11 11:52:14 +0100

    Time: 267 ms (5.93%), Space: 14.3 MB (14.37%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -9,6 +9,8 @@ class Solution:
         return [i[1] for i in w2s[:k]]
 
 #Makes a list for final answer, calling it w2s (weakest to strongest). We then make a for loop, in which we assign the row (i) and the value within the row (m), and then use the "enumerate" function, which then labels the row as a integer number going upwards from 0. We then say that the strength of the row is the sum of the values within the row, and assign the row number to it. We then place the list of strengths into the w2s list made earlier. From here, we sort the strength from lowest to highest. Finally, we print out the list of row strength, but change it so that it prints out the row number for i in the w2s row k.
+
+#LINE BREAKDOWN
         
 #w2s = [] - makes list to use later
         
