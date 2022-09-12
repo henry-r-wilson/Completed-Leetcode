d9fca872004fcdf082d722955c72a4e712188bce
Henry Wilson <113286807+henry-r-wilson@users.noreply.github.com>
2022-09-12 09:55:34 +0100

    Time: 1223 ms (5.01%), Space: 26.1 MB (28.47%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -6,6 +6,20 @@ class Solution:
                 return True
             num_set.add(i)
         return False
+
+#This code logs the numbers into a new set, and then checks to see if the numbers repeat themselves witin that set.
+
+#num_set = set() - Creates a set in which the numbers can be logged and then checked
+
+#for i in nums: - States to check for all numbers in the list
+
+#if i in num_set: - States that if a number i is found inside the number set, do action
+
+#return True - Action is to return value as true
+            
+#num_set.add(i) - Adds the next number into the subset to be checked
+        
+#return False - If no values duplicate, return False
     
 
                 
